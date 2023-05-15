24117c2001c6b4d4277bace843513150cef69a65
Pablo Ezequiel Balanzino <pbalanzino@gmail.com>
2023-05-15 19:40:24 -0300

    finalizado

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,10 @@ import '../scss/styles.scss';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
+
+let date = document.getElementById('date');
+
+document.addEventListener('click', (e) => {
+  if (e.target.id == 'date') {
+  }
+});
